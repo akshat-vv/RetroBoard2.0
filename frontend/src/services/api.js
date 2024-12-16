42c6e2dfdfad55c3b034b17cc4fab3b2a5a6c964
akshat-thg <Akshat.V62@thg.com>
2024-12-16 16:15:32 +0530

    changed the backend URI

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api";
+const API_URL = "https://retroboard2-0.onrender.com/api";
 
 const login = async (email, password) => {
   const requestBody = {
